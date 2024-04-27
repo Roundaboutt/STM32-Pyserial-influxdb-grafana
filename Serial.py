37062b0d469450a88d8666fcc4c3a1534ecc4f51
Roundaboutt <93021080+Roundaboutt@users.noreply.github.com>
2024-04-27 14:11:44 +0800

    Update Serial.py

--- Serial.py
+++ Serial.py
@@ -2,7 +2,7 @@ import serial
 from influxdb import InfluxDBClient
 
 host, port = "192.168.1.101", 8086
-# 如果没有开启用户名和密码，则不需要填写这两个参数
+//本地ip
 
 username, password = "admin", "symantec9098"
 database = "STM32"
@@ -10,7 +10,7 @@ client = InfluxDBClient(host=host, username=username, port=port, database=databa
 client.drop_database(database)
 client.create_database('STM32')
 # 连接串口
-serial = serial.Serial('COM5', 9600, timeout=2)
+serial = serial.Serial('COM5', 9600, timeout=2)//在设备管理器查看对应的COM端口号
 if serial.isOpen():
     print('串口已打开')
 
